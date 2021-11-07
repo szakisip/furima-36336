08702e6b366b13467c1890f9cfb2a960f4eef9a5
szakisip <ssg2012.yamato.s@gmail.com>
2021-11-07 12:39:56 +0900

    ログイン、ログアウト、エラー表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
+  # before_action :authenticate_user!
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -8,4 +10,9 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_FURIMA_USER"] && password == ENV["BASIC_FURIMA_PASSWORD"]
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :last_name, :first_name, :last_rename, :first_rename, :birthday])
+  end
+
 end
