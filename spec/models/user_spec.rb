a9cb514899cd4191de23b4e5f6c92f5474c84dea
szakisip <ssg2012.yamato.s@gmail.com>
2021-11-14 01:32:11 +0900

    モデルのテストコードを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,22 +16,29 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include("Email can't be blank")
     end
+    it "重複したemailが存在する場合登録できない" do
+      @user.save
+      another_user = FactoryBot.build(:user, email:@user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include("Email has already been taken")
+    end
     it "passwordが空だと登録できない" do
       @user.password = ''
       @user.valid?
       expect(@user.errors.full_messages).to include("Password can't be blank")
     end
-    # it 'passwordが5文字以下であれば登録できない' do
-    #   @user.nickname = 'aaaaaa'
-    #   @user.valid?
-    #   binding.pry
-    #   expect(@user.errors.full_messages).to include("Nickname can't be blank")
-    # end
-    # it "passwordとpassword_confirmationが不一致では登録できない" do
-    #   @user.nickname = ''
-    #   @user.valid?
-    #   expect(@user.errors.full_messages).to include("Nickname can't be blank")
-    # end
+    it 'passwordが5文字以下であれば登録できない' do
+      @user.password = '12345'
+      @user.password_confirmation = '12345'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end
+    it "passwordとpassword_confirmationが不一致では登録できない" do
+      @user.password = '123456'
+      @user.password_confirmation = '1234567'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
     it "last_nameが空だと登録できない" do
       @user.last_name = ''
       @user.valid?
@@ -56,11 +63,6 @@ RSpec.describe User, type: :model do
       @user.birthday = ''
       @user.valid?
       expect(@user.errors.full_messages).to include("Birthday can't be blank")
-    end
-    it "重複したemailが存在する場合登録できない" do
-      @user.nickname = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Nickname can't be blank")
     end
   end
 end
