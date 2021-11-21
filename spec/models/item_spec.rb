e262bd093493545acbc57f58cd95d47baddc2255
szakisip <ssg2012.yamato.s@gmail.com>
2021-11-22 03:28:47 +0900

    単体テストコード記載

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,125 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item =FactoryBot.create(:item)
+    sleep(1)
+  end
+
+  describe '商品出品登録' do
+    context '商品出品登録できるとき' do
+      it '全ての入力事項が、存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+      it 'カテゴリーが"---"以外であれば登録できる' do
+        @item.category_id = 2
+        expect(@item).to be_valid
+      end
+      it '商品の状態が"---"以外であれば登録できる' do
+        @item.condition_id = 2
+        expect(@item).to be_valid
+      end
+      it '配送料の負担が"---"以外であれば登録できる' do
+        @item.shipping_charge_id = 2
+        expect(@item).to be_valid
+      end
+      it '発送元の地域が"---"以外であれば登録できる' do
+        @item.state_id = 2
+        expect(@item).to be_valid
+      end
+      it '発送までの日数"---"以外であれば登録できる' do
+        @item.shipping_day_id = 2
+        expect(@item).to be_valid
+      end
+      it '価格が半角数字かつ300~9,999,999円以上であれば登録できる' do
+        @item.price = 400
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品出品登録できないとき' do
+      it '画像が空白では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名が空白では登録できない' do
+        @item.product_name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product name can't be blank")
+      end
+      it '商品の説明が空白では登録できない' do
+        @item.description = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'カテゴリーが空白では登録できない' do
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'カテゴリーが空白では登録できない' do
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      
+      it '商品の状態が空白では登録できない' do
+        @item.condition_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it '商品の状態が空白では登録できない' do
+        @item.condition_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+
+      it '配送料の負担が空白では登録できない' do
+        @item.shipping_charge_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping charge can't be blank")
+      end
+      it '配送料の負担が空白では登録できない' do
+        @item.shipping_charge_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping charge can't be blank")
+      end
+
+      it '発送元の地域が空白では登録できない' do
+        @item.state_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("State can't be blank")
+      end
+      it '発送元の地域が空白では登録できない' do
+        @item.state_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("State can't be blank")
+      end
+
+      it '発送までの日数が空白では登録できない' do
+        @item.shipping_day_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+      end
+      it '発送までの日数が空白では登録できない' do
+        @item.shipping_day_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+      end
+
+      it '価格が空白では登録できない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it '価格が空白では登録できない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      
+    end
+
+  end
 end
