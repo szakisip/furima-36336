9c321b61dd4bea770922210c139baa0c08ecd811
szakisip <ssg2012.yamato.s@gmail.com>
2021-11-24 08:06:28 +0900

    destroyアクションをコメントアウト、不要なコメント削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,14 +20,14 @@ class ItemsController < ApplicationController
   end
 
 
-  def destroy
-    # ログインしているユーザーと同一であればデータを削除する
-    if @item.user_id == current_user.id
-      @item.destroy
-      redirect_to root_path
-    else
-      redirect_to root_path
-    end
+  # def destroy
+  #   # ログインしているユーザーと同一であればデータを削除する
+  #   if @item.user_id == current_user.id
+  #     @item.destroy
+  #     redirect_to root_path
+  #   else
+  #     redirect_to root_path
+  #   end
   end
 
   private
