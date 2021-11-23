7ae98cc9033249b2f9e43f07b970d5ea81b4635b
szakisip <ssg2012.yamato.s@gmail.com>
2021-11-23 18:22:38 +0900

    商品一覧表示追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   # before_action :move_to_index, except: [:index, :show]
 
   def index
-    # @items = ITem.order("created_at DESC")
+    @items = Item.includes(:user).order("created_at DESC")
   end
 
   def new
@@ -16,6 +16,17 @@ class ItemsController < ApplicationController
       redirect_to root_path
     else
       render :new
+    end
+  end
+
+
+  def destroy
+    # ログインしているユーザーと同一であればデータを削除する
+    if @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
     end
   end
 
