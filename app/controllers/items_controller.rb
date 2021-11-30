510c22db22f9c6fbdc0b73c7949a7401e0184f96
szakisip <ssg2012.yamato.s@gmail.com>
2021-11-30 16:42:37 +0900

    商品情報編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,22 +22,22 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  # def edit
-  #   @item = Item.find(params[:id])
-  #   if @item.user_id == current_user.id
-  #   else
-  #     redirect_to root_path
-  #   end
-  # end
+  def edit
+    @item = Item.find(params[:id])
+    if @item.user_id == current_user.id
+    else
+      redirect_to root_path
+    end
+  end
 
-  # def update
-  #   @item = Item.find(params[:id])
-  #   if @item.update(item_params)
-  #     redirect_to item_path(item_params)
-  #   else
-  #     render 'edit'
-  #   end
-  # end
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(item_params)
+    else
+      render 'edit'
+    end
+  end
 
   # def destroy
   #   # ログインしているユーザーと同一であればデータを削除する
